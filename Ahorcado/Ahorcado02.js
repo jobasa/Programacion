1fb63c71077cd438b683bd33ab4c5584de3d3b82
ALUMNADO\jobasa <jobanyuls@gmail.com>
2017-10-05 10:16:31 +0200

    Actividad ahorcado

---
+++ Ahorcado/Ahorcado02.js
@@ -0,0 +1,10 @@
+var word="n";
+console.log(word);
+var  pantalla=prompt("Introduce una letra");
+
+
+while(pantalla!=word){
+var pantalla=prompt("Introduce otra letra");
+}
+alert("Has acertado");
+console.log("funciona");
