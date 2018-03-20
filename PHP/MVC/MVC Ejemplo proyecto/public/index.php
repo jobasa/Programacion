aa6fcdbe316cbc656bcfee776d282ec91cd6a4b3
ALUMNADO\jobasa <jobanyuls@gmail.com>
2018-03-20 12:07:19 +0100

    MVC

---
+++ PHP/MVC/MVC Ejemplo proyecto/public/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+use Daw\models\Table as Table;
+use Daw\config\config;
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pagina home del proyecto</title>
+  </head>
+  <body>
+    <?php
+    //Ejemplo de uso de modelos
+    $t=new Table();
+    ?>
+  </body>
+</html>
---
+++ PHP/MVC/MVC Ejemplo proyecto/src/config/config.php
@@ -0,0 +1,17 @@
+<?php
+namespace Daw\config;
+
+/**
+ *
+ */
+class config
+{
+
+  function __construct(argument)
+  {
+    echo "Clase config";
+  }
+}
+
+
+ ?>
