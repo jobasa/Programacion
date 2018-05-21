6ae8cc2cac08e9d2685c7b33385314e5dbc3aa88
DESKTOP-A5CERV0\Joan <jobanyuls@gmail.com>
2018-05-21 19:05:15 +0200

    Proyecto09

---
+++ PHP/MVC/Proyecto09/public/ahorcado.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ahorcado</title>
+    <style media="screen">
+      body{
+        background-color: #008080;
+      }
+      h1{
+        border-bottom-style:inset;
+        border-color: #000000 ;
+        width: 300px;
+      }
+      div{
+        font-size: 150%;
+      }
+    </style>
+  </head>
+  <body>
+    <h1 style="background-color: #008080">Bienvenido al ahorcado, a jugar!</h1>
+    <form id="formularioPrueba" action="#" method="post">
+      <div><p id="dibujo" value=""></p></div>
+      <div>Introduce una letra:<br>
+      <input type="text"  size=4 id="entrada" value="">
+     <input type="text" id="verificar" value="" readonly> </div>
+      <div><p id="errores" value=""></p>
+      <p id="intentos" value=""></p>
+      <p id="resultado" value=""></p></div>
+      <p><button type="button" name="Comprobar" onclick="comprobar()">Comprobar</button></p>
+    </form>
+      <script type="text/javascript" src=js/Ahorcado.js></script>
+  </body>
+</html>
--- PHP/MVC/Proyecto09/public/inicio.php
+++ PHP/MVC/Proyecto09/public/inicio.php
@@ -7,6 +7,7 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" type="text/css" href="css/insertarUsuarios.css">
   </head>
   <body>
     <h1><center>EL AHORCADO</center></h1>
@@ -14,10 +15,10 @@
 
     <fieldset>
         <legend>ESCOJE UN USUARIO</legend>
-        <form class="" action="index.html" method="post">
+        <form class="" action="ahorcado.php" method="post">
           <select class="" name="usuario">
             <option value=""> seleciona usuario</option>
-          </select>
+          </select><br>
           <input type="submit" name="empezar" value="Empezar">
         </form>
     </fieldset>
